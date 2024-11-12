7230516485802612dfe1999784dab628d9958bb3
fan <3223643263@qq.com>
2024-11-12 12:54:56 +0800

    1112

--- img2gad_pytorch/my2.sh
+++ img2gad_pytorch/my2.sh
@@ -9,8 +9,8 @@ Saverootdir=$3
 Classes='0_real 1_fake'
 
 # 定义测试集数据集类别
-Testdatas='glide_100_10 ldm_200_cfg glide_50_27 ldm_100 glide_100_27 dalle ldm_200 guided'
-
+# Testdatas='glide_100_10 ldm_200_cfg glide_50_27 ldm_100 glide_100_27 dalle ldm_200 guided'
+Testdatas='guided'
 # 定义测试集图像根目录和保存目录
 Testrootdir=${Imgrootdir}
 Savedir=$Saverootdir/test/
@@ -21,7 +21,7 @@ do
     for Class in $Classes
     do
         Imgdir=${Testdata}/${Class}
-        CUDA_VISIBLE_DEVICES=$1 python $GANmodelpath/gen_imggrad.py \
+        CUDA_VISIBLE_DEVICES=$1 python $GANmodelpath/gen_imggrad2.py \
             ${Testrootdir}${Imgdir} \
             ${Savedir}${Imgdir}_grad \
             ./karras2019stylegan-bedrooms-256x256_discriminator.pth \
